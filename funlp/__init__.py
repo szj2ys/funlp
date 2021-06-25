3da4982d84d3dabe5956ed8c8bbc38414692341b
阿不拉多耶基奇 <15574364821@163.com>
2021-06-25 17:11:28 +0800

    20210625 17:11:28

--- funlp/__init__.py
+++ funlp/__init__.py
@@ -1,7 +1,11 @@
 #-*- encoding:utf-8 -*-
-from __future__ import absolute_import
+from __future__ import absolute_import, print_function
+import warnings
 from .TextRank4Sentence import TextRank4Sentence
 from .TextRank4Word import TextRank4Word
 from . import utils
 
 from .__version__ import version, __version__
+
+warnings.filterwarnings('ignore')
+warnings.simplefilter('ignore')
